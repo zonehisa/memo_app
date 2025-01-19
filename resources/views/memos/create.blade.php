ee1d22057298fb8ba6bf266caf7c05b2ee3ca68b
zonehisa <nijipapazoneokun123@gmail.com>
2025-01-19 18:22:42 +0900

    日本語化まで完了

--- resources/views/memos/create.blade.php
+++ resources/views/memos/create.blade.php
@@ -12,15 +12,30 @@
 <body>
     <a href="{{ route('memos.index') }}">戻る</a>
     <h1>新規登録</h1>
+
+    @if ($errors->any())
+        <div class="error">
+            <p>
+                <b>{{ count($errors) }}件のエラーがあります。</b>
+            </p>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        
+    @endif
+
     <form action="{{ route('memos.store') }}" method="post">
         @csrf
         <p>
             <label for="title">タイトル</label><br>
-            <input type="text" name="title" id="title">
+            <input type="text" name="title" id="title" value="{{ old('title') }}">
         </p>
         <p>
             <label for="body">本文</label><br>
-            <textarea name="body" class="body" id="body"></textarea>
+            <textarea name="body" class="body" id="body">{{ old('body') }}</textarea>
         </p>
         <input type="submit" value="登録">
     </form>
