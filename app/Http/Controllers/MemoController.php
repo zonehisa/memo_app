640a90bf36a6412b082f04139c61cf8e1518411d
zonehisa <nijipapazoneokun123@gmail.com>
2025-01-19 17:20:22 +0900

    削除機能を追加しました

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -63,4 +63,11 @@ class MemoController extends Controller
         // 登録したらindexに戻る
         return redirect(route('memos.index'));
     }
+    public function destroy($id)
+    {
+        $memo = Memo::find($id);
+        $memo->delete();
+
+        return redirect(route('memos.index'));
+    }
 }
--- resources/views/memos/show.blade.php
+++ resources/views/memos/show.blade.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>memo show</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
+
 <body>
     <a href="{{ route('memos.index') }}">戻る</a>
     <h1>{{ $memo->title }}</h1>
     <p>{!! nl2br(e($memo->body)) !!}</p>
 
-
+    <div class="button-group">
+        <!-- $memoのidを元に編集ページへ遷移する -->
+        <button onclick='location.href="{{ route("memos.edit", $memo) }}"'>編集する</button>
+        <form action="{{ route('memos.destroy', $memo) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="削除する" onclick="if(!confirm('削除しますか？')){return false};">
+        </form>
+    </div>
 </body>
+
 </html>
