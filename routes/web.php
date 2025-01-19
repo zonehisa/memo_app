7e67693a586552c3d52c6929a760fdc7b5b6f37a
zonehisa <nijipapazoneokun123@gmail.com>
2025-01-19 11:28:54 +0900

    プロジェクトの準備完了

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('memos', MemoController::class);
+// リソースを使用しない場合
+// Route::get('/memos', [MemoController::class, 'index']);
+// Route::get('/memos/create', [MemoController::class, 'create']);
+// Route::post('/memos', [MemoController::class, 'store']);
+// Route::get('/memos/{memo}', [MemoController::class, 'show']);
+// Route::get('/memos/{memo}/edit', [MemoController::class, 'edit']);
+// Route::patch('/memos/{memo}', [MemoController::class, 'update']);
+// Route::delete('/memos/{memo}', [MemoController::class, 'destroy']);
