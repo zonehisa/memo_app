85a44ebd4bd0d074e703e338a20ce7eb05427593
zonehisa <nijipapazoneokun123@gmail.com>
2025-01-19 12:10:17 +0900

    照会機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Memo;
 use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
-    //
+    // showページへ移動
+    public function show($id)
+    {
+        $memo = Memo::find($id);
+        return view('memos.show', ['memo' => $memo]);
+    }
 }
